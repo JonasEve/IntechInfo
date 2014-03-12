1bb9c9c143363783d51617f1cb1c528542bd4243
JE <jevenisse@nomalys.com>
2014-03-12 10:20:55 +0100

    fix internal security

--- FirstSolution/Intech.Business/FileProcessorResult.cs
+++ FirstSolution/Intech.Business/FileProcessorResult.cs
@@ -7,14 +7,14 @@ namespace Intech.Business
 {
     public class FileProcessorResult
     {
-        public int TotalFileCount { get; set; }
+        public int TotalFileCount { get; internal set; }
 
-        public int TotalDirectoryCount { get; set; }
+        public int TotalDirectoryCount { get; internal set; }
 
-        public int HiddenFileCount { get; set; }
+        public int HiddenFileCount { get; internal set; }
 
-        public int HiddenDirectoryCount { get; set; }
+        public int HiddenDirectoryCount { get; internal set; }
 
-        public int UnaccessibleFileCount { get; set; }
+        public int UnaccessibleFileCount { get; internal set; }
     }
 }
