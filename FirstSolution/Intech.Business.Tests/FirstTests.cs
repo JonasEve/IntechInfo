79ba3a260f570111d6ab8789d2a3fa81b57dc489
JE <jevenisse@nomalys.com>
2014-03-13 09:32:00 +0100

    unit test

---
+++ FirstSolution/Intech.Business.Tests/FirstTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intech.Business.Tests
+{
+    [TestFixture]
+    public class FirstTests
+    {
+        [Test]
+        public void Test1()
+        {
+            //Arrange
+            int firstNumber = 1;
+            int secondNumber = 2;
+            int sum;
+
+            //Act
+            sum = firstNumber + secondNumber;
+
+            //Assert
+            Assert.That(sum > firstNumber && sum > secondNumber);
+        }
+
+        [Test]
+        public void WhereAmI()
+        {
+            DirectoryInfo directory = TestHelper.SolutionFolder;
+        }
+    }
+}
